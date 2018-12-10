fa4e5aa6680bf0d386cc39b0b9574fa6d16e926a
keprom <kokshetau.energo.prom@mail.ru>
2018-12-10 10:31:06 +0600

    fixed adding tp

--- system/application/controllers/billing.php
+++ system/application/controllers/billing.php
@@ -332,13 +332,15 @@ function dob($text)
 		$this->load->view("right");
 		
 	}
-	function adding_tp()
-	{
-		if (trim($_POST['name'])!="")
-			$this->db->insert('industry.tp',$_POST);
-		redirect("billing/tp");
-		
-	}
+
+    function adding_tp()
+    {
+        if (trim($_POST['name'])!=""){
+            $_POST['ture_id'] = isset($_POST['ture_id'])? $_POST['ture_id']: 0;
+            $this->db->insert('industry.tp',$_POST);
+        }
+        redirect("billing/tp");
+    }
 
 	function banks()
 	{
